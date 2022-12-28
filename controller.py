8310830af0e57f623b065e5a7dc6dc61a3eaf848
Dima <dmitriy_evdokimov94@mail.ru>
2022-12-28 21:20:23 +0300

    task_10

---
+++ controller.py
@@ -0,0 +1,44 @@
+import user_interface as user
+import modul_sum as modul
+import modul_div 
+import modul_mult
+import modul_sub
+
+def button_click():
+    print('Комплексное = 1, Рациональное = 2')
+    value_item = int(input('Выберите значение: '))
+
+    if value_item == 1:
+        value_a = user.input_complex()
+        value_b = user.input_complex()
+
+    if value_item == 2:
+        value_a = user.input_data()
+        value_b = user.input_data()
+
+    print('Выберите функцию: 1: Деление 2: Умноженик 3: Сложение 4: Вычитание')
+
+    value_modul = int(input('выберите значение: '))
+
+    if value_modul == 3:
+        modul.init(value_a, value_b)
+        result = modul.do_it()
+        user.view_data(result)
+
+    if value_modul == 1:
+        modul_div.init(value_a, value_b)
+        result = modul_div.div()
+        user.view_data(result)
+
+    if value_modul == 2:
+        modul_mult.init(value_a, value_b)
+        result = modul_mult.do_it()
+        user.view_data(result)
+    if value_modul == 4:
+        modul_sub.init(value_a, value_b)
+        result = modul_sub.do_it()
+        user.view_data(result)
+    
+
+
+
